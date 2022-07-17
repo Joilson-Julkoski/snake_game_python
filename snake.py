74ff47bed4c5d742d552d6d12628f573054ded9a
Joilson-Julkoski <joilson7548@gmail.com>
2022-07-16 22:49:49 -0300

    Apos o game over se clicar r restarta

--- snake.py
+++ snake.py
@@ -26,8 +26,10 @@ macax = random.randint(0, 600) // grid * grid
 macay = random.randint(0, 600) // grid * grid
 
 font = pygame.font.Font('freesansbold.ttf', 40)
+font2 = pygame.font.Font('freesansbold.ttf', 20)
 
 text = font.render('GAME OVER', True, (0, 0, 0))
+text2 = font2.render('press "R" for restart', True, (255, 255, 255))
 
 while True:
     relogio.tick(15)
@@ -53,15 +55,21 @@ while True:
                 direcaoy = grid
 
     if corpo[-1] in corpo[:-1] or corpo[-1][0] < 0 or corpo[-1][0] > largura_tela or corpo[-1][1] < 0 or corpo[-1][1] > altura_tela: #Colisão com o corpo
-        while True:
+        gameover = True
+        while gameover:
 
             pygame.draw.rect(tela, (255, 0, 0), (5, altura_tela//2-50, largura_tela-10, 100))
             tela.blit(text, (largura_tela//2-120, altura_tela//2-15, largura_tela-10, 100))
+            tela.blit(text2, (largura_tela//2-90, altura_tela//2+25, largura_tela, 100))
             pygame.display.update()
             for evento in pygame.event.get():
                 if evento.type == QUIT:
                     pygame.quit()
                     exit()
+                if evento.type == KEYDOWN:
+                    if evento.key == K_r:
+                        corpo = [[200, 200], [210, 200], [220, 200]]
+                        gameover = False
 
     if corpo[-1] == [macax, macay]:
         corpo.append([corpo[-1][0] + direcaox, corpo[-1][1] + direcaoy])
